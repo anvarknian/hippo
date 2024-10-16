f36d7dc7e37c0d292962e90a52c7a6bf5390d5aa
Anwar Knyane <21956329+anvarknian@users.noreply.github.com>
2024-10-16 02:07:13 -0300

    - add spark for last 2 tasks - code optimization/refactoring

--- app/app.py
+++ app/app.py
@@ -1,9 +1,13 @@
 from collections import defaultdict
 
-from spark_proc import create_dataframe
+from pyspark.sql import Window
+from pyspark.sql import functions as F
+
 from models.claims import Claims
 from models.pharmacies import Pharmacies
 from models.reverts import Reverts
+from spark_proc import create_dataframe, claims_schema
+from spark_proc import pharmacies_schema, reverts_schema
 from utils.readers import read_claims, read_pharmacies, read_reverts
 from utils.writers import save_to_json
 
@@ -11,7 +15,6 @@ OUTPUT_PATH = './output'
 
 
 def filter_pharmacy_events(p: Pharmacies, c: Claims):
-    """Filter out Claims that belong to valid Pharmacies (by NPI)."""
     valid_pharmacies = {pharmacy.npi for pharmacy in p.pharmacies}
     filtered_claims = [cm for cm in c.claims if cm.npi in valid_pharmacies]
     return Claims(**{'claims': filtered_claims})
@@ -57,15 +60,53 @@ def compute_metrics(c: Claims, r: Reverts):
 def run(claims_path: str, reverts_path: str, pharmacies_path: str):
     print(f"Reading from: {claims_path}, {reverts_path}, {pharmacies_path}")
 
-    # Read/Validation of data.
+    # 1 - Read data stored in JSON files + Validation
     claims = read_claims(claims_path)
     reverts = read_reverts(reverts_path)
     pharmacies = read_pharmacies(pharmacies_path)
 
-    # Compute metrics
+    # Filter metrics
     valid_claims = filter_pharmacy_events(pharmacies, claims)
-    metrics = compute_metrics(valid_claims, reverts)
-    save_to_json(f'{OUTPUT_PATH}/metrics.json', metrics)
 
-    df = create_dataframe(valid_claims.to_json())
-    df.show()
+    # 2 - Calculate metrics for some dimensions
+    metrics = compute_metrics(valid_claims, reverts)
+    save_to_json(f'{OUTPUT_PATH}/metrics-2.json', metrics)
+
+    # Spark Data
+    claims_dict = claims.model_dump(mode='json').get('claims')
+    pharmacies_dict = pharmacies.model_dump(mode='json').get('pharmacies')
+    reverts_dict = reverts.model_dump(mode='json').get('reverts')
+    claims_df = create_dataframe(claims_dict, claims_schema)
+    pharmacies_df = create_dataframe(pharmacies_dict, pharmacies_schema)
+    reverts_df = create_dataframe(reverts_dict, reverts_schema)
+    valid_claims_df = claims_df.join(
+        reverts_df, claims_df.id == reverts_df.claim_id, "left_anti"
+    )
+    claims_with_chain_df = valid_claims_df.join(
+        pharmacies_df, valid_claims_df.npi == pharmacies_df.npi, "inner"
+    ).select(valid_claims_df["*"], pharmacies_df.chain).cache()
+
+    # 3 -  Make a recommendation for the top 2 Chain to be displayed for each Drug.
+    avg_price_df = claims_with_chain_df.groupBy("ndc", "chain") \
+        .agg(F.avg((F.col("price") / F.col("quantity"))).alias("avg_price")) \
+        .orderBy("ndc", "avg_price")
+
+    top_chains_df = avg_price_df.withColumn("rank", F.row_number().over(
+        Window.partitionBy("ndc").orderBy("avg_price"))) \
+        .filter(F.col("rank") <= 2) \
+        .select("ndc", "chain", "avg_price")
+
+    final_df = top_chains_df.groupBy("ndc") \
+        .agg(F.collect_list(F.struct(
+        F.col("chain").alias("name"),
+        F.col("avg_price")
+    )).alias("chain"))
+    final_df.write.mode("overwrite").json(f'{OUTPUT_PATH}/metrics-3.json')
+
+    # 4 - Understand Most common quantity prescribed for a given Drug
+    quantity_counts_df = claims_with_chain_df.groupBy("ndc", "quantity") \
+        .count() \
+        .orderBy("ndc", F.desc("count"))
+    most_common_quantities_df = quantity_counts_df.groupBy("ndc") \
+        .agg(F.collect_list("quantity").alias("most_prescribed_quantity"))
+    most_common_quantities_df.write.mode("overwrite").json(f'{OUTPUT_PATH}/metrics-4.json')
--- app/spark_proc.py
+++ app/spark_proc.py
@@ -1,12 +1,34 @@
 from typing import List, Dict
 
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StructType, StructField, StringType, FloatType
 
 spark = SparkSession.builder \
     .appName("sparkcomputejob") \
     .config("spark.driver.bindAddress", "localhost") \
     .getOrCreate()
 
-def create_dataframe(data: List[Dict], schema=None):
+
+def create_dataframe(data, schema):
     return spark.createDataFrame(data, schema=schema)
 
+
+claims_schema = StructType([
+    StructField("id", StringType(), True),
+    StructField("ndc", StringType(), True),
+    StructField("npi", StringType(), True),
+    StructField("quantity", FloatType(), True),
+    StructField("price", FloatType(), True),
+    StructField("timestamp", StringType(), True)
+])
+
+pharmacies_schema = StructType([
+    StructField("chain", StringType(), True),
+    StructField("npi", StringType(), True)
+])
+
+reverts_schema = StructType([
+    StructField("id", StringType(), True),
+    StructField("claim_id", StringType(), True),
+    StructField("timestamp", StringType(), True)
+])
